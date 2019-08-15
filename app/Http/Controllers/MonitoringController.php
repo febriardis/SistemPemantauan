f8fa1b1341088407e79829e7afc92d01c587d2b5
febriardis <ardisaputrafebri@gmail.com>
2019-08-15 10:13:06 +0700

    edit-uno

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -344,3 +344,351 @@ class MonitoringController extends Controller
 //     delay(30000);  //Send a request every 30 seconds
    
 //   }
+
+
+// #include<SoftwareSerial.h>
+// #include<ArduinoJson.h>
+// #include<OneWire.h>
+// #include<DallasTemperature.h>
+// #include<LiquidCrystal_I2C.h>
+// #include<Wire.h>
+// #include <elapsedMillis.h>
+
+// elapsedMillis sincePrint1, sincePrint2;
+// LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3,POSITIVE);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+// SoftwareSerial S_SERIAL(3,5);
+
+// #define ONE_WIRE_BUS 2
+// OneWire oneWire(ONE_WIRE_BUS);
+// DallasTemperature sensorSuhu(&oneWire);
+
+// const int PIN_PH = A0;
+// const int PIN_TURB = A1;
+// const int pinSaklar = 6;
+// const int pinLed = 4;
+// const int pinLed2 = 7;
+// int pinBuzzer = 8;
+// int kondisiSaklar = 0;
+// float ph, temperature, turbidity;
+// String ket,status_air;
+
+// float nilTemp = 0;
+// int adcPH = 0;
+// int adcTurb = 0;
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(9600);
+//   S_SERIAL.begin(9600);
+//   sensorSuhu.begin();
+
+//   pinMode(pinSaklar, INPUT);
+//   pinMode(3, INPUT);
+//   pinMode(5, OUTPUT);
+//   pinMode(pinLed, OUTPUT);
+//   pinMode(pinLed2, OUTPUT);
+//   pinMode(pinBuzzer, OUTPUT);
+
+//   //lcd
+//   lcd.begin(20, 4);
+// }
+
+// void loop() {
+//   kondisiSaklar = digitalRead(pinSaklar);
+//   if(kondisiSaklar == HIGH){
+//     digitalWrite(pinLed, LOW);
+//     Serial.println("lampu nyala");
+//     get_data(); //get function get data from sensor
+//     fuzzy_logic(); //get function fuzzy logic
+//     delay(200); 
+//   }else{
+//     digitalWrite(pinLed, HIGH);
+//     if (sincePrint1 > 5000){     //eksekusi per 5 detik
+//       // ------ turbidity sensor code --------
+//       sincePrint1 = 0;
+//       sensorSuhu.requestTemperatures();
+//       nilTemp = nilTemp + sensorSuhu.getTempCByIndex(0);
+//       adcPH = adcPH + analogRead(PIN_PH);
+//       adcTurb = adcTurb + analogRead(PIN_TURB);
+//     }
+//     else if(sincePrint2 > 30000){ //eksekusi per 30 detik
+//       Serial.println("lampu mati");
+//       sincePrint2 = 0;
+    
+//       temperature = nilTemp / 6;
+      
+//       int adcP = adcPH / 6;
+//       float vol_ph = 5 / 1023.0 * adcP;
+//       ph = 7 + ((2.5 - vol_ph)/0.18);
+      
+//       int adcT = adcTurb/6;
+//       float vol_turb = adcT * (5.0 / 1024);
+//       turbidity = 100.0 - (vol_turb / 4.25) * 100.00;
+      
+//       nilTemp = 0;
+//       adcTurb = 0;
+//       adcPH = 0;
+      
+//       fuzzy_logic(); //memanggil fungsi fuzzy logic
+//     }
+//   }
+// }
+
+// void get_data(){
+//   // ------ ph sensor code ----------
+//   int adcPH = analogRead(PIN_PH);
+//   float vol_ph = 5 / 1023.0 * adcPH;
+//   ph = 7 + ((2.5 - vol_ph)/0.18);
+
+//   // ------ temperature sensor code -------
+//   sensorSuhu.requestTemperatures();
+//   temperature = sensorSuhu.getTempCByIndex(0);
+  
+//   // ------ turbidity sensor code --------
+//   int val = analogRead(PIN_TURB);
+//   float vol_turb = val * (5.0 / 1024);
+//   turbidity = 100.0 - (vol_turb / 4.25) * 100.00;
+// }
+
+// void fuzzy_logic(){
+//   float nTemp[3];
+//   float nPH[3];
+//   float nTurbidity[3];
+//   float aPred, z;
+   
+//   // ---------------- fuzzy logic code -------------------
+//   // Fuzifikasi
+//   // ---------------- variabel suhu ------------------
+//   // fungsi keanggotaan suhu dingin
+//   if(temperature < 26){
+//       nTemp[0] = 1;
+//   }
+//   else if(temperature >= 26 && temperature <= 28){
+//       nTemp[0] = (28 - temperature)/2;
+//   }
+//   else if(temperature > 28){
+//       nTemp[0] = 0;
+//   }
+  
+//   // fungsi keanggotaan suhu normal
+//   if(temperature > 30 || temperature < 26){
+//       nTemp[1] = 0;
+//   }
+//   else if(temperature >= 26 && temperature <= 28){
+//       nTemp[1] = (temperature - 26) / 2;
+//   }
+//   else if(temperature > 28 && temperature <= 30){
+//       nTemp[1] = (30 - temperature) / 2;
+//   }
+  
+//   //fungsi keanggotaaan suhu panas
+//   if(temperature <= 28){
+//       nTemp[2] = 0;
+//   }else if(temperature > 28 && temperature <= 30){
+//       nTemp[2] = (temperature-28)/2;
+//   }
+//   else if (temperature > 30){
+//       nTemp[2] = 1;
+//   }
+
+//   //------------------ Variabel pH -----------------
+//   //fungsi keanggotaan ph rendah
+//   if(ph < 6.5){
+//       nPH[0] = 1;
+//   }
+//   else if (ph >= 6.5 && ph <= 6.8) {
+//       nPH[0] = (6.8 - ph) / 0.3;
+//   }
+//   else if (ph > 6.8) {
+//       nPH[0] = 0;
+//   }
+//   //fungsi keanggotaan ph normal
+//   if(ph > 7 || ph < 6.5){
+//       nPH[1] = 0;
+//   }
+//   else if(ph >= 6.5 && ph <= 6.8){
+//       nPH[1] = (ph - 6.5) / 0.3;
+//   }
+//   else if(ph > 6.8 && ph <= 7){
+//       nPH[1] = (7 - ph) / 0.2;
+//   }
+  
+//   //fungsi keanggotaan ph tinggi
+//   if(ph <= 6.8){
+//       nPH[2]=0;
+//   }
+//   else if (ph>6.8 && ph<7){
+//       nPH[2]=(ph-6.8)/0.3;
+//   }
+//   else if (ph>=7){ 
+//       nPH[2]=1;
+//   }
+  
+//   //------------------ Variabel Kekeruhan -----------------
+//   //fungsi keanggotaan kekeruhan normal
+//   if(turbidity > 25){
+//       nTurbidity[0] = 0;
+//   }
+//   else if (turbidity > 0 && turbidity <= 25){
+//       nTurbidity[0] = (25 - turbidity) / 25;
+//   }
+//   else if (turbidity <= 0){
+//       nTurbidity[0] = 1;
+//   }
+  
+//   //fungsi keanggotaan kekeruhan tinggi
+//   if(turbidity <= 0){
+//       nTurbidity[1] = 0;
+//   }
+//   else if (turbidity > 0 && turbidity <= 25){
+//       nTurbidity[1] = (turbidity - 0) / 25;
+//   }
+//   else if (turbidity >= 25){
+//       nTurbidity[1] = 1;
+//   }
+
+//   // rule base
+//   if(nTemp[0]==1 && nPH[0]==1 && nTurbidity[0]!=0){
+//       ket   = "suhu dan ph rendah";
+//       aPred = min(nTemp[0], min(nPH[0], nTurbidity[0]));
+//       z     = 100 - (50*aPred);   
+//   }//[R1]
+//   else if (nTemp[0]==1 && nPH[1]!=0 && nTurbidity[0]!=0) {
+//       ket   = "suhu rendah";        
+//       aPred = min(nTemp[0], min(nPH[1], nTurbidity[0]));
+//       z     = 100 - (50*aPred);   
+//   }//[R2]
+//   else if (nTemp[0]==1 && nPH[2]==1 && nTurbidity[0]!=0) {
+//       ket   = "suhu rendah, dan ph tinggi";   
+//       aPred = min(nTemp[0], min(nPH[2], nTurbidity[0]));
+//       z     = 100 - (50*aPred);          
+//   }//[R3]
+//   else if (nTemp[0]==1 && nPH[0]==1 && nTurbidity[1]==1) {
+//       ket   = "suhu dan ph rendah, serta kekeruhan tinggi"; 
+//       aPred = min(nTemp[0], min(nPH[0], nTurbidity[1]));
+//       z     = 100 - (50*aPred);           
+//   }//[R4]
+//   else if (nTemp[0]==1 && nPH[1]!=0 && nTurbidity[1]==1) {
+//       ket   = "suhu rendah dan kekeruhan tinggi";   
+//       aPred = min(nTemp[0], min(nPH[1], nTurbidity[1]));
+//       z     = 100 - (50*aPred);        
+//   }//[R5]
+//   else if (nTemp[0]==1 && nPH[2]==1 && nTurbidity[1]==1) {
+//       ket   = "suhu rendah, ph dan kekeruhan tinggi";  
+//       aPred = min(nTemp[0], min(nPH[2], nTurbidity[1]));
+//       z     = 100 - (50*aPred);          
+//   }//[R6]
+//   else if (nTemp[1]!=0 && nPH[0]==1 && nTurbidity[0]!=0) {
+//       ket   = "ph rendah";   
+//       aPred = min(nTemp[1], min(nPH[0], nTurbidity[0]));
+//       z     = 100 - (50*aPred);      
+//   }//[R7]
+//   else if (nTemp[1]!=0 && nPH[1]!=0 && nTurbidity[0]!=0) {
+//       ket   = "nilai parameter normal";  
+//       aPred = min(nTemp[1], min(nPH[1], nTurbidity[0]));
+//       z     = abs(1 - (aPred*50)); 
+//   }//[R8]
+//   else if (nTemp[1]!=0 && nPH[2]==1 && nTurbidity[0]!=0) {
+//       ket   = "ph tinggi";  
+//       aPred = min(nTemp[1], min(nPH[2], nTurbidity[0]));
+//       z     = 100 - (50*aPred); 
+//   }//[R9]
+//   else if (nTemp[1]!=0 && nPH[0]==1 && nTurbidity[1]==1) {
+//       ket   = "ph rendah dan kekeruhan tinggi";  
+//       aPred = min(nTemp[1], min(nPH[0], nTurbidity[1]));
+//       z     = 100 - (50*aPred); 
+//   }//[R10]
+//   else if (nTemp[1]!=0 && nPH[1]!=0 && nTurbidity[1]==1) {
+//       ket   = "kekeruhan tinggi";  
+//       aPred = min(nTemp[1], min(nPH[1], nTurbidity[1]));
+//       z     = 100 - (50*aPred); 
+//   }//[R11]
+//   else if (nTemp[1]!=0 && nPH[2]==1 && nTurbidity[1]==1) {
+//       ket   = "ph dan kekeruhan tinggi";  
+//       aPred = min(nTemp[1], min(nPH[2], nTurbidity[1]));
+//       z     = 100 - (50*aPred); 
+//   }//[R12]
+//   else if (nTemp[2]==1 && nPH[0]==1 && nTurbidity[0]!=0) {
+//       ket   = "suhu tinggi dan ph rendah";  
+//       aPred = min(nTemp[2], min(nPH[0], nTurbidity[0]));
+//       z     = 100 - (50*aPred); 
+//   }//[R13]
+//   else if (nTemp[2]==1 && nPH[1]!=0 && nTurbidity[0]!=0) {
+//       ket   = "suhu tinggi";  
+//       aPred = min(nTemp[2], min(nPH[1], nTurbidity[0]));
+//       z     = 100 - (50*aPred); 
+//   }//[R14]
+//   else if (nTemp[2]==1 && nPH[2]==1 && nTurbidity[0]!=0) {
+//       ket   = "suhu dan ph tinggi";  
+//       aPred = min(nTemp[2], min(nPH[2], nTurbidity[0]));
+//       z     = 100 - (50*aPred); 
+//   }//[R15]
+//   else if (nTemp[2]==1 && nPH[0]==1 && nTurbidity[1]==1) {
+//       ket   = "ph rendah, serta suhu dan kekeruhan tinggi";  
+//       aPred = min(nTemp[2], min(nPH[0], nTurbidity[1]));
+//       z     = 100 - (50*aPred); 
+//   }//[R16]
+//   else if (nTemp[2]==1 && nPH[1]!=0 && nTurbidity[1]==1) {
+//       ket   = "suhu dan kekeruhan tinggi";  
+//       aPred = min(nTemp[2], min(nPH[1], nTurbidity[1]));
+//       z     = 100 - (50*aPred); 
+//   }//[R17]
+//   else if (nTemp[2]==1 && nPH[2]==1 && nTurbidity[1]==1) {
+//       ket   = "suhu, ph, dan kekeruhan tinggi";  
+//       aPred = min(nTemp[2], min(nPH[2], nTurbidity[1]));
+//       z     = 100 - (50*aPred); 
+//   }//[R18]
+  
+//   // defuzifikasi
+//   int zT = (aPred*z)/aPred;
+  
+//   // keterangan
+//   if(zT>=0 && zT<50){
+//       status_air = "air masih layak!!";
+//   }else if(zT >= 50){
+//       status_air = "air tidak layak !!";
+// //      tone(pinBuzzer, 300, 10000);
+//   }
+    
+//   post_mcu(); //get function post data too nodemcu
+//   show_lcd(); //get function show data in lcd
+// }
+
+// //function post data too nodemcu
+// void post_mcu(){
+//   // ------ arduino json code ----------
+//   const size_t capacity = JSON_OBJECT_SIZE(3);
+//   DynamicJsonBuffer jsonBuffer(capacity);
+  
+//   JsonObject& root = jsonBuffer.createObject();
+//   root["temperature"] = temperature;
+//   root["ph"] = ph;
+//   root["turbidity"] = turbidity; 
+//   root["status"] = status_air;
+//   root["information"] = ket;
+//   root.prettyPrintTo(Serial);
+//   root.printTo(S_SERIAL);
+
+// //  menyalakan lampu led
+//   digitalWrite(pinLed, LOW);
+//   delay(200);
+//   digitalWrite(pinLed, HIGH);
+//   delay(200);
+// }
+
+// //function show data in lcd
+// void show_lcd(){
+//   //show data in LCD
+//   lcd.setCursor(0,0);
+//   lcd.print("Suhu:");
+//   lcd.print(temperature,2);
+//   lcd.print(", pH:");
+//   lcd.print(ph,2);
+//   lcd.setCursor(0,1);
+//   lcd.print("Kekeruhan:");
+//   lcd.print(turbidity);                                                                  
+//   lcd.setCursor(0,2); 
+//   lcd.print("=====STATUS AIR=====");
+//   lcd.setCursor(0,3);
+//   lcd.print(status_air);
+// }
