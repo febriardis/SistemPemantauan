9cdd4a09afead4844bc4cb9f3ffb8e9d3bb93c07
Febri A. Saputra <febriardis>
2019-10-12 17:38:04 +0700

    edit-ss

--- config/cors.php
+++ config/cors.php
@@ -12,20 +12,19 @@ return [
     |
     */
     
-    'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
-    'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
-    'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
-    'exposedHeaders' => [],
-    'maxAge' => 0,
-   
     // 'supportsCredentials' => false,
-    // // 'allowedOrigins' => ['http://localhost:8080', 'https://sip.billionairecoach.co.id'],
     // 'allowedOrigins' => ['*'],
-    // 'allowedOriginsPatterns' => [],
-    // 'allowedHeaders' => ['*'],
-    // 'allowedMethods' => ['*'],
+    // 'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
+    // 'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
     // 'exposedHeaders' => [],
     // 'maxAge' => 0,
+
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
 
 ];
